4f7691c4eac988e92e04acbd48602c53c85dd908
JaiGoel12 <jaigoel1201@gmail.com>
2025-02-13 00:16:12 +0530

    Dashboard completed

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,11 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import Listing from './_components/Listing'
 
 const Dashboard = () => {
   return (
     <div>
-      
+      <Listing/>
     </div>
   )
 }
